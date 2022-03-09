43835bf99e5dad6f6c4ab692d18e396f7f6e4491
JoeReis <32462325+JoeReis1983@users.noreply.github.com>
2022-03-08 21:10:45 -0300

    O inico

---
+++ backend/src/main/java/celso/reis/App.java
@@ -0,0 +1,13 @@
+package celso.reis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
