4b792510585fd664f998d852de595ff8a5beb8fc
JoeReis <32462325+JoeReis1983@users.noreply.github.com>
2022-03-29 08:55:17 -0300

    iniciando back, tentativa 1

---
+++ celso/src/main/java/com/agoravai/celso/CelsoApplication.java
@@ -0,0 +1,13 @@
+package com.agoravai.celso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelsoApplication.class, args);
+	}
+
+}
---
+++ celso/src/test/java/com/agoravai/celso/CelsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agoravai.celso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
