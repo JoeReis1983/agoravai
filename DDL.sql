4602968b64976c7e3e58f34fb53321d323350bdb
JoeReis1983 <joeprimeiro@hotmail.com>
2022-03-29 11:30:01 +0000

    alteração do banco de dados

--- DDL.sql
+++ DDL.sql
@@ -1,10 +1,10 @@
-create schema anotacao;
+create schema agoravai;
 
-use anotacao;
+use agoravai;
 
 create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on anotacao.* to user@'localhost';
+grant select, insert, delete, update on agoravai.* to user@'localhost';
 
 create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
@@ -13,24 +13,27 @@ create table usr_usuario (
   primary key (usr_id),
   unique key uni_usuario_nome (usr_nome)
 );
-
-create table aut_autorizacao (
-  aut_id bigint unsigned not null auto_increment,
-  aut_nome varchar(20) not null,
-  primary key (aut_id),
-  unique key uni_aut_nome (aut_nome)
-);
-
-create table uau_usuario_autorizacao (
-  usr_id bigint unsigned not null,
-  aut_id bigint unsigned not null,
-  primary key (usr_id, aut_id),
-  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
-  foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
-);
-
 insert into usr_usuario (usr_nome, usr_senha)
     values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
-insert into aut_autorizacao (aut_nome)
-    values ('ROLE_ADMIN');
-insert into uau_usuario_autorizacao values (1, 1);
+insert into usr_usuario (usr_nome, usr_senha)
+    values ('celso', 'celso');
+-- create table aut_autorizacao (
+--   aut_id bigint unsigned not null auto_increment,
+--   aut_nome varchar(20) not null,
+--   primary key (aut_id),
+--   unique key uni_aut_nome (aut_nome)
+-- );
+
+-- create table uau_usuario_autorizacao (
+--   usr_id bigint unsigned not null,
+--   aut_id bigint unsigned not null,
+--   primary key (usr_id, aut_id),
+--   foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+--   foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+-- );
+
+-- insert into usr_usuario (usr_nome, usr_senha)
+--     values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+-- insert into aut_autorizacao (aut_nome)
+--     values ('ROLE_ADMIN');
+-- insert into uau_usuario_autorizacao values (1, 1);
