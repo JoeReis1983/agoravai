f8f5c202515c9b2fdc07c97fb7a910b38c554bc6
JoeReis1983 <joeprimeiro@hotmail.com>
2022-04-04 12:29:01 +0000

    criação de roles para usuarios

--- DDL.sql
+++ DDL.sql
@@ -10,13 +10,14 @@ create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
   usr_nome varchar(20) not null,
   usr_senha varchar(100) not null,
+  usr_role varchar(10) not null,
   primary key (usr_id),
   unique key uni_usuario_nome (usr_nome)
 );
-insert into usr_usuario (usr_nome, usr_senha)
-    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
-insert into usr_usuario (usr_nome, usr_senha)
-    values ('celso', 'celso');
+insert into usr_usuario (usr_nome, usr_senha,usr_role)
+    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C','admin');
+insert into usr_usuario (usr_nome, usr_senha,usr_role)
+    values ('celso', 'celso','user');
 -- create table aut_autorizacao (
 --   aut_id bigint unsigned not null auto_increment,
 --   aut_nome varchar(20) not null,
