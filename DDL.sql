d412dcc685d1e2d27eb59d1fbbf35170ce57af0e
JoeReis1983 <joeprimeiro@hotmail.com>
2022-06-10 21:35:33 +0000

    corrompeu o bd

--- DDL.sql
+++ DDL.sql
@@ -1,12 +1,12 @@
 drop user 'user'@'localhost';
-drop schema agenda;
-create schema agenda;
+drop schema agoravai;
+create schema agoravai;
 
-use agenda;
+use agoravai;
 
 create user 'user'@'localhost' identified by 'pass123';
 
-grant select, insert, delete, update on agenda.* to user@'localhost';
+grant select, insert, delete, update on agoravai.* to user@'localhost';
 
 create table usr_usuario (
   usr_id bigint unsigned not null auto_increment,
@@ -39,11 +39,29 @@ create table ant_trabalho (
   unique key ant_trabalho_titulo (ant_titulo)
 );
 
+create table alu_aluno (
+  alu_id bigint unsigned not null auto_increment,
+  alu_nome varchar(100) not null,
+  alu_p1 varchar(5) not null,
+  alu_p2 varchar(5) not null,
+  alu_trabalho varchar(100) not null,
+  primary key (alu_id),
+  unique key alu_aluno_nome (alu_nome)
+);
+
+
+
 insert into usr_usuario (usr_nome, usr_senha)
     values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
 insert into aut_autorizacao (aut_nome)
     values ('ROLE_ADMIN');
+
+  insert into usr_usuario (usr_nome, usr_senha)
+    values ('user', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+insert into aut_autorizacao (aut_nome)
+    values ('ROLE_USUARIO');
 insert into uau_usuario_autorizacao values (1, 1);
+insert into uau_usuario_autorizacao values (2, 2);
 
 insert into ant_trabalho (ant_titulo, ant_texto)
     values ('teste', 'teste');
